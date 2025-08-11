36e0239d869698a7dfd2b7bdd362971d4eaf2f3f
Arthur Schiller <arthurschiller10@gmail.com>
2025-08-11 10:45:22 -0300

    exemplo de herança e polimorfismo

---
+++ 11.08/veiculo/src/model/Carro.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Carro extends Veiculo{
+
+    public String acelerar() {
+        return "Acelerando o carro";
+    }
+
+    public String frear() {
+        return "Freando o carro";
+    }
+
+
+}
---
+++ 11.08/veiculo/src/model/Veiculo.java
@@ -0,0 +1,9 @@
+package model;
+
+public abstract class Veiculo {
+
+    public abstract String acelerar();
+
+    public abstract String frear();
+
+}
