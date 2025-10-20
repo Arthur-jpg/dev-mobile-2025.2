f9e04be1368afe31712a89cd1d4f5c7dc1fd6114
Arthur Schiller <arthurschiller10@gmail.com>
2025-10-20 11:52:32 -0300

    final

--- intets/app/src/main/java/com/example/intents/MainActivity.kt
+++ intets/app/src/main/java/com/example/intents/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.intents
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -20,6 +22,14 @@ class MainActivity : AppCompatActivity() {
         btAvancar.setOnClickListener {
             nome = etNome.text.toString()
             val intent = Intent(this, Activity2::class.java)
+            intent.putExtra("Nome", nome)
+            startActivity(intent)
+        }
+
+        val btCahamada = findViewById<Button>(R.id.btChamadaTelefonica)
+        btCahamada.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = ("tel:" + "190").toUri()
             startActivity(intent)
         }
     }
